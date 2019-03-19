ad569f0d8c151ba156481d328bf6aafa5dc8c87d
Idan Tene <idan.tene@gmail.com>
2019-03-19 16:51:33 +0200

    Example for a simple view that reads json files

---
+++ src/explorer.ts
@@ -0,0 +1,32 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class MockExplorer implements vscode.TreeDataProvider<vscode.TreeItem> {
+    // private watcher = vscode.workspace.createFileSystemWatcher("*.json");
+    private contents = new Array<vscode.TreeItem>();
+    private _onDidChangeTreeData = new vscode.EventEmitter<vscode.TreeItem>();
+    onDidChangeTreeData = this._onDidChangeTreeData.event;
+
+    constructor() {
+        this.hardRefresh();
+    }
+
+    getTreeItem(element: vscode.TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+        return element;
+    }
+
+    getChildren(element?: vscode.TreeItem | undefined): vscode.ProviderResult<vscode.TreeItem[]> {
+        return Promise.resolve(this.contents);
+    }
+
+    private hardRefresh() {
+        vscode.workspace.findFiles("**/*.json").then(files => {
+            this.contents = files.map(f => {
+                const shortPath = path.join(path.basename(path.dirname(f.fsPath)), path.basename(f.fsPath));
+                return new vscode.TreeItem(shortPath);
+            });
+            this._onDidChangeTreeData.fire();
+        });
+    }
+}

eb682322419bb8e057029499145e51c622e634e2
Idan Tene <idan.tene@gmail.com>
2019-03-20 12:45:47 +0200

    Adds lookup of credentials in home folder

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
 import * as ini from "ini";
+import * as os from "os";
 import { MockExplorer } from "./explorer";
 
 export function activate(context: vscode.ExtensionContext) {
@@ -10,16 +11,30 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.window.registerTreeDataProvider("mocksExplorer", jsonExplorer);
 	vscode.commands.registerCommand("helloWorld.console", () => console.log("HELLO THERE"));
 
-	let refreshToken: string | undefined = vscode.workspace.getConfiguration("unmock").refreshToken;
-	if (refreshToken === null && vscode.workspace.rootPath !== undefined) {
-		const unmockPath = vscode.workspace.getConfiguration("unmock").path;
-		const credentialFile = path.join(vscode.workspace.rootPath, unmockPath, "credentials");
-		if (fs.existsSync(credentialFile)) {
-			const iniContents = ini.parse(fs.readFileSync(credentialFile, 'utf-8'));
-			refreshToken = iniContents.unmock.token;
+	const config = vscode.workspace.getConfiguration("unmock");
+	let refreshToken: string | undefined = config.refreshToken;
+
+	if (refreshToken === null) { // Not set in configuration, look for it in other places
+		const homeDirs = [];
+		if (vscode.workspace.rootPath !== undefined) { // First preference to workspace home directory
+			homeDirs.push(vscode.workspace.rootPath);
 		}
+		homeDirs.push(os.homedir()); // Second preference to user home directory
+		const unmockPath = config.path;
+		let foundCredentialsFile = false;
+		homeDirs.forEach(homedir => {
+			if (!foundCredentialsFile) {
+				const credentialFile = path.join(homedir, unmockPath, "credentials");
+				if (fs.existsSync(credentialFile)) {
+					const iniContents = ini.parse(fs.readFileSync(credentialFile, 'utf-8'));
+					refreshToken = iniContents.unmock.token;
+					foundCredentialsFile = true;
+				}
+			}
+		});
+		// save refresh token in configuration in workspace
+		config.update("refreshToken", refreshToken, false);
 	}
-	console.log(refreshToken);
 }
 
 // this method is called when your extension is deactivated

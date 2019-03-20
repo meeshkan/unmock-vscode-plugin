8c4ced58e2d12bc2668693e9cd83603730be727e
Idan Tene <idan.tene@gmail.com>
2019-03-20 10:21:32 +0200

    TreeView for json files only

--- src/explorer.ts
+++ src/explorer.ts
@@ -2,31 +2,91 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 
-export class MockExplorer implements vscode.TreeDataProvider<vscode.TreeItem> {
+export class MockExplorer implements vscode.TreeDataProvider<MockTreeItem> {
     // private watcher = vscode.workspace.createFileSystemWatcher("*.json");
-    private contents = new Array<vscode.TreeItem>();
-    private _onDidChangeTreeData = new vscode.EventEmitter<vscode.TreeItem>();
+    private locationOfJsons = new Array<string>();
+    private _onDidChangeTreeData = new vscode.EventEmitter<MockTreeItem>();
     onDidChangeTreeData = this._onDidChangeTreeData.event;
 
     constructor() {
-        this.hardRefresh();
-    }
-
-    getTreeItem(element: vscode.TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        return element;
-    }
-
-    getChildren(element?: vscode.TreeItem | undefined): vscode.ProviderResult<vscode.TreeItem[]> {
-        return this.contents;
-    }
-
-    private hardRefresh() {
-        vscode.workspace.findFiles("**/*.json").then(files => {
-            this.contents = files.map(f => {
-                const shortPath = path.join(path.basename(path.dirname(f.fsPath)), path.basename(f.fsPath));
-                return new vscode.TreeItem(shortPath);
-            });
+        // TODO: Also add a watcher?
+        vscode.workspace.findFiles("**/*.json").then(filepaths => {
+            filepaths.forEach(fp => this.locationOfJsons.push(fp.fsPath));
             this._onDidChangeTreeData.fire();
         });
     }
+
+    getTreeItem(element: MockTreeItem): MockTreeItem | Thenable<MockTreeItem> {
+        return element;
+    }
+
+    getChildren(element?: MockTreeItem | undefined): vscode.ProviderResult<MockTreeItem[]> {
+        if (element === undefined) {
+            return this.populateChildren(vscode.workspace.rootPath);
+        }
+        return this.populateChildren(element.currentPath, element);
+    }
+
+    private populateChildren(rootDir?: string, parent?: MockTreeItem) {
+        if (rootDir === undefined) {
+            return [];
+        }
+        const files = this.getJsons(rootDir);
+        return files.map(f => {
+            const label = MockExplorer.trimToFolderOrFileBeneath(f, rootDir);
+            const isFile = fs.statSync(f).isFile();
+            const ti = new MockTreeItem(label, isFile ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Expanded);
+            ti.fullPath = f;
+            if (parent !== undefined) {
+                ti.parent = parent;
+            }
+            ti.currentPath = path.join(rootDir, label);
+            return ti;
+        });
+    }
+
+    private getJsons(rootDir?: string) {
+        /**
+         * rootDir - either undefined or relative path such as ".unmock/" or ".unmock/save/"
+         */
+        if (rootDir === undefined) {
+            return [];
+        }
+        const dir = rootDir ? (rootDir.endsWith("/") ? rootDir : rootDir + "/") : "";
+        if (this.directoryIsKnownToContainJson(dir) === false) {
+            return [];
+        }
+        const filePaths = fs.readdirSync(dir);
+        return filePaths.map(relativePath => path.join(dir, relativePath)).filter(fp => {
+            if (this.directoryIsKnownToContainJson(fp) === false) {
+                return false;
+            }
+            const fileStats = fs.statSync(fp);
+            return (fileStats.isFile() && fp.endsWith(".json")) || fileStats.isDirectory();
+        });
+    }
+
+    private directoryIsKnownToContainJson(directory: string) {
+        let directoryIsKnownToContainJson = false;
+        this.locationOfJsons.forEach(location => {
+            directoryIsKnownToContainJson = directoryIsKnownToContainJson || location.indexOf(directory) > -1;
+        });
+        return directoryIsKnownToContainJson;
+    }
+
+    private static trimToFolderOrFileBeneath(filepath: string, rootDir?: string) {
+        /**
+         * Returns the label as the file or foldername immediately after given rootDir
+         * rootDir is expected to be an absolute path.
+         */
+        const noRoot = rootDir ? filepath.replace(rootDir, "") : filepath;
+        const splittedPath = noRoot.split("/");
+        return splittedPath[1];  // 1 because the trailing '/' remains after replace
+    }
+}
+
+export class MockTreeItem extends vscode.TreeItem {
+    public fullPath: string | undefined;
+    public currentPath: string | undefined;
+    public parent: MockTreeItem | undefined;
 }

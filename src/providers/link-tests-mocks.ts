938ea79fd7c14a603debb33b5df54041b454f74f
Idan Tene <idan.tene@gmail.com>
2019-04-23 12:41:12 +0300

    Refactor snap collection

--- src/providers/link-tests-mocks.ts
+++ src/providers/link-tests-mocks.ts
@@ -10,6 +10,86 @@ import { removeJSCommentsFromSourceText,
 import { ITestSnap } from "../interfaces";
 import { mockExplorer } from "../index";
 
+class SnapCollection { 
+  private snapsLocations: string[] = [];
+  private snaps: {[snapLocation: string]: Snap} = {};
+
+  constructor() {
+    this.refreshSnaps();
+  }
+
+  private refreshSnaps() {
+    this.snaps = {};
+    vscode.workspace.findFiles(`**/*${SNAPSHOT_SUFFIX}`)
+      .then(files => {
+        if (files === undefined || files.length === 0) {
+          return;
+        }
+        // Check if any of the found snap files have a "hash" keyword...
+        files.forEach((file) => {
+          const contents = fs.readFileSync(file.fsPath, "utf-8");
+          const hasKeywords = SNAPSHOT_KEYWORDS.reduce((agg, kw) => agg = agg && contents.includes(kw), true);
+          if (hasKeywords) {
+            this.snapsLocations.push(file.fsPath);
+          }
+        });
+      });
+  }
+
+  public snapFromFilename(filename: string): Snap | undefined {
+    const name = path.basename(filename);
+    if (this.snaps[name] !== undefined) {
+      return this.snaps[name];
+    }
+    const filtered = this.snapsLocations.filter(snapLoc => path.basename(snapLoc, SNAPSHOT_SUFFIX) === name);
+    if (filtered) {
+      this.snaps[name] = new Snap(filtered[0]);
+      return this.snaps[name];
+    }
+    return;
+  }
+}
+
+class Snap {
+  private snapContents: any;
+  public tests: string[];
+
+  constructor(private snapFile: string) {
+    this.snapContents = require(snapFile); // Load contents from snap file
+    // Get test names listed in this snap file
+    // 1. Get keys from the loaded snap object
+    // 2. Remove the last word from each key (assumed to be a number)
+    // 3. uniq the result
+    this.tests = _.uniq(Object.keys(this.snapContents).map(k => k.split(" ").slice(0, -1).join(" ")));
+  }
+
+  public loadTest({idx, name} : {idx?: number, name?: string}) {
+    let testName: string;
+    if (name === undefined) {
+      if (idx === undefined || idx < 0 || idx >= this.tests.length) {
+        throw Error("Invalid index for test snapshot.");
+      }
+  
+      testName = this.tests[idx];
+    } else {
+      testName = name;
+    }
+    return Object.keys(this.snapContents)
+      .filter(key => key.startsWith(testName))
+      .map(key => JSON.parse(this.snapContents[key]));
+  }
+}
+
+const SNAP_COLLECTION = new SnapCollection();
+
+export class LinkMockCodeLens implements vscode.CodeLensProvider {
+  onDidChangeCodeLenses?: vscode.Event<void> | undefined;
+
+  provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens[]> {
+    throw new Error("Method not implemented.");
+  }
+}
+
 // TODO is hover the best idea? Do we want a code-lens type, to show next to the matching calls, etc?
 export class LinkMockHoverProvider implements vscode.HoverProvider {
   snaps: vscode.Uri[] = [];
@@ -18,14 +98,11 @@ export class LinkMockHoverProvider implements vscode.HoverProvider {
                position: vscode.Position,
                token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {
 
-    this.getSnapFiles(); // one-time getter to find snapshot files
-    const file = this.snapFile(document.fileName); // find snapshot file that matches current document
-    if (file === undefined) {
+    const snap = SNAP_COLLECTION.snapFromFilename(document.fileName); // find snapshot file that matches current document
+    if (snap === undefined) {
       return;
     }
-    const snap = new Snap(file.fsPath); // Load snapshot by creating the Snap object
     const srcText = removeJSCommentsFromSourceText(document.getText());
-
     // Find the ranges for different tests that are recorded in the snapshot
     const relevantRanges = findRangesFromTestNames(srcText, snap.tests);
     // Test if any of the ranges apply for current position; otherwise we don't need to show information
@@ -54,33 +131,6 @@ export class LinkMockHoverProvider implements vscode.HoverProvider {
     });
     content.isTrusted = true;
     return new vscode.Hover(content);
-  }
-
-  private getSnapFiles() {
-    if (this.snaps.length === 0) {
-      vscode.workspace.findFiles(`**/*${SNAPSHOT_SUFFIX}`)
-        .then(files => {
-          if (files === undefined || files.length === 0) {
-            return;
-          }
-          // Check if any of the found snap files have a "hash" keyword...
-          files.forEach((file) => {
-            const contents = fs.readFileSync(file.fsPath, "utf-8");
-            const hasKeywords = SNAPSHOT_KEYWORDS.reduce((agg, kw) => agg = agg && contents.includes(kw), true);
-            if (hasKeywords) {
-              this.snaps.push(file);
-            }
-          });
-        });
-    }
-  }
-
-  private snapFile(filename: string) {
-    const filtered = this.snaps.filter(snapFile => path.basename(snapFile.fsPath, SNAPSHOT_SUFFIX) === path.basename(filename));
-    if (filtered) {
-      return filtered[0];
-    }
-    return;
   }
 }
 
@@ -146,34 +196,4 @@ function findRangesFromTestNames(rawContent: string, uniqueTests: string[]) {
   const testNameLocations = getRangeFromTextAndMatch(rawContent, uniqueTests, false);
   // returned range spans from beginning of the given line to the location of matching '}'...
   return testNameLocations.map(location => findBalancedCurlyBraces(location, splitContent));
-}
-
-class Snap {
-  private snapContents: any;
-  public tests: string[];
-
-  constructor(private snapFile: string) {
-    this.snapContents = require(snapFile); // Load contents from snap file
-    // Get test names listed in this snap file
-    // 1. Get keys from the loaded snap object
-    // 2. Remove the last word from each key (assumed to be a number)
-    // 3. uniq the result
-    this.tests = _.uniq(Object.keys(this.snapContents).map(k => k.split(" ").slice(0, -1).join(" ")));
-  }
-
-  public loadTest({idx, name} : {idx?: number, name?: string}) {
-    let testName: string;
-    if (name === undefined) {
-      if (idx === undefined || idx < 0 || idx >= this.tests.length) {
-        throw Error("Invalid index for test snapshot.");
-      }
-  
-      testName = this.tests[idx];
-    } else {
-      testName = name;
-    }
-    return Object.keys(this.snapContents)
-      .filter(key => key.startsWith(testName))
-      .map(key => JSON.parse(this.snapContents[key]));
-  }
 }

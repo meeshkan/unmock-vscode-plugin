1fc91b31a38dd9395848128d3d4982bce39fd95d
Idan Tene <idan.tene@gmail.com>
2019-04-24 14:34:01 +0300

    Show hover when clicking on codelens

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,22 @@ import * as fs from "fs";
 import * as path from "path";
 import * as ini from "ini";
 import * as os from "os";
-import { IInsertUnmockAction, IMockLocation } from "./interfaces";
+import { IMockLocation } from "./interfaces";
 import { MockExplorer } from "./explorer";
-import { TypescriptInsertUnmockCodeLens,
-         TypeScriptInsertUnmockAction,
-         InsertUnmockHoverProvider } from "./providers/insert-unmock";
-import { LinkMockHoverProvider } from "./providers/link-tests-mocks";
-import { getImportStatement, getTestCalls, getConfig, AllJSFileFilters } from "./utils";
-
-const insertUnmockToTest = (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: IInsertUnmockAction[]) => {
-  // Adds unmock to a test file
-  if (args.length === 0) { // No args given
-    return;
-  }
-  const argsObj = args[0];
-  // Adds the import statement in the specified `unmockImportLocation` under `args`
-  textEditor.insertSnippet(new vscode.SnippetString(`${getImportStatement(argsObj.lang)}\n`),
-                           argsObj.unmockImportLocation);
-  const lastImportLocation = argsObj.lastImportLocation;
-  // Add the beforeEach and afterEach calls after the last import
-  // +1 to add after the last import statement, +1 to account for the addition of the unmock import
-  const afterLastImport = new vscode.Range(lastImportLocation.line + 2, 0, lastImportLocation.line + 2, 0);
-  textEditor.insertSnippet(new vscode.SnippetString(`\n${getTestCalls(argsObj.lang)}\n`), afterLastImport);
-};
+import {
+  TypescriptInsertUnmockCodeLens,
+  TypeScriptInsertUnmockAction,
+  InsertUnmockHoverProvider,
+  insertUnmockToTest,
+} from "./providers/insert-unmock";
+import { LinkMockHoverProvider, LinkMockCodeLens, SnapCollection } from "./providers/link-tests-mocks";
+import { getConfig, AllJSFileFilters } from "./utils";
 
 const updateRefreshToken = (config: vscode.WorkspaceConfiguration) => {
   let refreshToken: string | undefined;
   const homeDirs = [];
-  if (vscode.workspace.rootPath !== undefined) { // First preference to workspace home directory
+  if (vscode.workspace.rootPath !== undefined) {
+    // First preference to workspace home directory
     homeDirs.push(vscode.workspace.rootPath);
   }
   homeDirs.push(os.homedir()); // Second preference to user home directory
@@ -40,7 +28,7 @@ const updateRefreshToken = (config: vscode.WorkspaceConfiguration) => {
     if (!foundCredentialsFile) {
       const credentialFile = path.join(homedir, unmockPath, "credentials");
       if (fs.existsSync(credentialFile)) {
-        const iniContents = ini.parse(fs.readFileSync(credentialFile, 'utf-8'));
+        const iniContents = ini.parse(fs.readFileSync(credentialFile, "utf-8"));
         refreshToken = iniContents.unmock.token;
         foundCredentialsFile = true;
       }
@@ -52,43 +40,75 @@ const updateRefreshToken = (config: vscode.WorkspaceConfiguration) => {
 
 export let mockExplorer: undefined | MockExplorer;
 
+const disposables: vscode.Disposable[] = [];
+
 export function activate(context: vscode.ExtensionContext) {
   const config = getConfig();
 
-  vscode.commands.registerTextEditorCommand("unmock.insertUnmockToTest", insertUnmockToTest);
+  // This is needed for the Uri used in markdowns
+  disposables.push(vscode.commands.registerTextEditorCommand("unmock.insertUnmockToTest", insertUnmockToTest));
 
-  if (config.codeLens === true) {  // Add the CodeLens suggestions (if enabled)
-    vscode.languages.registerCodeLensProvider(AllJSFileFilters, new TypescriptInsertUnmockCodeLens());
+  disposables.push(
+    vscode.commands.registerTextEditorCommand(
+      "unmock.showMockLinkHover",
+      (editor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) => {
+        const { range } = args[0];
+        const curSelection = editor.selection;
+        editor.selection = new vscode.Selection(range.start, range.start);
+        vscode.commands.executeCommand("editor.action.showHover").then(() => (editor.selection = curSelection));
+      }
+    )
+  );
+
+  if (config.codeLens === true) {
+    // Add the CodeLens suggestions (if enabled)
+    disposables.push(vscode.languages.registerCodeLensProvider(AllJSFileFilters, new TypescriptInsertUnmockCodeLens()));
   }
-  if (config.lightbulb === true) { // Add the lightbulb suggestions (if enabled)
-    vscode.languages.registerCodeActionsProvider(AllJSFileFilters, new TypeScriptInsertUnmockAction());
+  if (config.lightbulb === true) {
+    // Add the lightbulb suggestions (if enabled)
+    disposables.push(
+      vscode.languages.registerCodeActionsProvider(AllJSFileFilters, new TypeScriptInsertUnmockAction())
+    );
   }
   // Register hover providers
-  vscode.languages.registerHoverProvider(AllJSFileFilters, new InsertUnmockHoverProvider());
-  vscode.languages.registerHoverProvider(AllJSFileFilters, new LinkMockHoverProvider());
+  disposables.push(vscode.languages.registerHoverProvider(AllJSFileFilters, new InsertUnmockHoverProvider()));
+
+  // For linking mocks and tests, we need a collection of snap files
+  const snapCollection = new SnapCollection(); // todo - cleanup?
+  disposables.push(vscode.languages.registerHoverProvider(AllJSFileFilters, new LinkMockHoverProvider(snapCollection)));
+  disposables.push(vscode.languages.registerCodeLensProvider(AllJSFileFilters, new LinkMockCodeLens(snapCollection)));
 
   // Add the extension button
-  mockExplorer = new MockExplorer();
-  vscode.window.registerTreeDataProvider("unmock.mocksExplorer", mockExplorer);
+  mockExplorer = new MockExplorer(); // todo - cleanup?
+  disposables.push(vscode.window.registerTreeDataProvider("unmock.mocksExplorer", mockExplorer));
 
   // Add individual commands
-	vscode.commands.registerCommand("unmock.editMock", (element: IMockLocation) => {  // Open mock from MockTreeItem
-		vscode.commands.executeCommand("vscode.open", vscode.Uri.file(element.currentPath));
-  });
+  disposables.push(
+    vscode.commands.registerCommand("unmock.editMock", (element: IMockLocation) => {
+      // Open mock from MockTreeItem
+      vscode.commands.executeCommand("vscode.open", vscode.Uri.file(element.currentPath));
+    })
+  );
 
-	// Create the statusbar section
-	const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-	vscode.commands.registerCommand("unmock.statusbar.hide", () => statusBar.hide());
-	vscode.commands.registerCommand("unmock.statusbar.text", (text: string) => {
-		statusBar.text = `Unmock: ${text}`;
-		statusBar.show();
-	});
-	statusBar.command = "unmock.statusbar.hide";
+  // Create the statusbar section
+  const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+  disposables.push(statusBar);
+  disposables.push(vscode.commands.registerCommand("unmock.statusbar.hide", () => statusBar.hide()));
+  disposables.push(
+    vscode.commands.registerCommand("unmock.statusbar.text", (text: string) => {
+      statusBar.text = `Unmock: ${text}`;
+      statusBar.show();
+    })
+  );
+  statusBar.command = "unmock.statusbar.hide";
 
-  if (config.refreshToken === null) { // Not set in configuration, look for it in other places
+  if (config.refreshToken === null) {
+    // Not set in configuration, look for it in other places
     updateRefreshToken(config);
-	}
+  }
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+// this method is called when the extension is deactivated
+export function deactivate() {
+  disposables.forEach(disp => disp.dispose());
+}
--- src/providers/link-tests-mocks.ts
+++ src/providers/link-tests-mocks.ts
@@ -2,38 +2,31 @@ import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
 import * as _ from "lodash";
-import { removeJSCommentsFromSourceText,
-         getRangeFromTextAndMatch,
-         removeStringsFromSourceText,
-         countInString,
-         findPositionInRanges } from "../utils";
-import { ITestSnap } from "../interfaces";
+import {
+  removeJSCommentsFromSourceText,
+  getRangeFromTextAndMatch,
+  removeStringsFromSourceText,
+  countInString,
+  findPositionInRanges,
+} from "../utils";
+import { ITestSnap, IBuildLinkTestMockHover } from "../interfaces";
 import { mockExplorer } from "../index";
 
-class SnapCollection { 
+const SNAPSHOT_SUFFIX = ".snap";
+// These must be found in a snapshot to be considered valid
+const SNAPSHOT_KEYWORDS = ['"hash":', '"host":', '"method":', '"path":'];
+
+export class SnapCollection {
   private snapsLocations: string[] = [];
-  private snaps: {[snapLocation: string]: Snap} = {};
+  private snaps: { [snapLocation: string]: Snap } = {};
+  private eventsToFire: Array<vscode.EventEmitter<any>> = [];
 
   constructor() {
     this.refreshSnaps();
   }
 
-  private refreshSnaps() {
-    this.snaps = {};
-    vscode.workspace.findFiles(`**/*${SNAPSHOT_SUFFIX}`)
-      .then(files => {
-        if (files === undefined || files.length === 0) {
-          return;
-        }
-        // Check if any of the found snap files have a "hash" keyword...
-        files.forEach((file) => {
-          const contents = fs.readFileSync(file.fsPath, "utf-8");
-          const hasKeywords = SNAPSHOT_KEYWORDS.reduce((agg, kw) => agg = agg && contents.includes(kw), true);
-          if (hasKeywords) {
-            this.snapsLocations.push(file.fsPath);
-          }
-        });
-      });
+  public addEventEmitter(ee: vscode.EventEmitter<any>) {
+    this.eventsToFire.push(ee);
   }
 
   public snapFromFilename(filename: string): Snap | undefined {
@@ -42,17 +35,38 @@ class SnapCollection {
       return this.snaps[name];
     }
     const filtered = this.snapsLocations.filter(snapLoc => path.basename(snapLoc, SNAPSHOT_SUFFIX) === name);
-    if (filtered) {
+    if (filtered.length !== 0) {
       this.snaps[name] = new Snap(filtered[0]);
       return this.snaps[name];
     }
     return;
   }
+
+  private refreshSnaps() {
+    this.snaps = {};
+    vscode.workspace.findFiles(`**/*${SNAPSHOT_SUFFIX}`).then(files => {
+      if (files === undefined || files.length === 0) {
+        return;
+      }
+      // Check if any of the found snap files have a "hash" keyword...
+      files.forEach((file, index) => {
+        const contents = fs.readFileSync(file.fsPath, "utf-8");
+        const hasKeywords = SNAPSHOT_KEYWORDS.reduce((agg, kw) => (agg = agg && contents.includes(kw)), true);
+        if (hasKeywords) {
+          this.snapsLocations.push(file.fsPath);
+        }
+        if (index === files.length - 1) {
+          // final file! fire all events...
+          this.eventsToFire.forEach(ee => ee.fire());
+        }
+      });
+    });
+  }
 }
 
-class Snap {
-  private snapContents: any;
+export class Snap {
   public tests: string[];
+  private snapContents: any;
 
   constructor(private snapFile: string) {
     this.snapContents = require(snapFile); // Load contents from snap file
@@ -60,16 +74,23 @@ class Snap {
     // 1. Get keys from the loaded snap object
     // 2. Remove the last word from each key (assumed to be a number)
     // 3. uniq the result
-    this.tests = _.uniq(Object.keys(this.snapContents).map(k => k.split(" ").slice(0, -1).join(" ")));
+    this.tests = _.uniq(
+      Object.keys(this.snapContents).map(k =>
+        k
+          .split(" ")
+          .slice(0, -1)
+          .join(" ")
+      )
+    );
   }
 
-  public loadTest({idx, name} : {idx?: number, name?: string}) {
+  public loadTest({ idx, name }: { idx?: number; name?: string }) {
     let testName: string;
     if (name === undefined) {
       if (idx === undefined || idx < 0 || idx >= this.tests.length) {
         throw Error("Invalid index for test snapshot.");
       }
-  
+
       testName = this.tests[idx];
     } else {
       testName = name;
@@ -80,25 +101,46 @@ class Snap {
   }
 }
 
-const SNAP_COLLECTION = new SnapCollection();
-
 export class LinkMockCodeLens implements vscode.CodeLensProvider {
-  onDidChangeCodeLenses?: vscode.Event<void> | undefined;
+  codeLensChangeEventEmitter = new vscode.EventEmitter<void>();
+  onDidChangeCodeLenses = this.codeLensChangeEventEmitter.event;
 
-  provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens[]> {
-    throw new Error("Method not implemented.");
+  constructor(private snapCollection: SnapCollection) {
+    // Fire the codelens when we have all the files ready...
+    this.snapCollection.addEventEmitter(this.codeLensChangeEventEmitter);
+  }
+
+  provideCodeLenses(
+    document: vscode.TextDocument,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.CodeLens[]> {
+    const snap = this.snapCollection.snapFromFilename(document.fileName); // find snapshot file that matches current document
+    if (snap === undefined) {
+      return;
+    }
+    const srcText = removeJSCommentsFromSourceText(document.getText());
+    const relevantRanges = findRangesFromTestNames(srcText, snap.tests);
+    return relevantRanges.map(
+      rng =>
+        new vscode.CodeLens(rng, {
+          command: "unmock.showMockLinkHover",
+          title: "Show Unmocked API calls",
+          arguments: [{ range: rng }],
+        })
+    );
   }
 }
 
 // TODO is hover the best idea? Do we want a code-lens type, to show next to the matching calls, etc?
 export class LinkMockHoverProvider implements vscode.HoverProvider {
-  snaps: vscode.Uri[] = [];
+  constructor(private snapCollection: SnapCollection) {}
 
-  provideHover(document: vscode.TextDocument,
-               position: vscode.Position,
-               token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {
-
-    const snap = SNAP_COLLECTION.snapFromFilename(document.fileName); // find snapshot file that matches current document
+  provideHover(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.Hover> {
+    const snap = this.snapCollection.snapFromFilename(document.fileName); // find snapshot file that matches current document
     if (snap === undefined) {
       return;
     }
@@ -106,42 +148,50 @@ export class LinkMockHoverProvider implements vscode.HoverProvider {
     // Find the ranges for different tests that are recorded in the snapshot
     const relevantRanges = findRangesFromTestNames(srcText, snap.tests);
     // Test if any of the ranges apply for current position; otherwise we don't need to show information
-    let matchingTestIndex = findPositionInRanges(position, relevantRanges);
-    if (matchingTestIndex === -1) { // None matching...
+    const matchingTestIndex = findPositionInRanges(position, relevantRanges);
+    if (matchingTestIndex === -1) {
+      // None matching...
       return;
     }
-    const relevantTestSnap: ITestSnap[] = snap.loadTest({idx: matchingTestIndex}); // Loads the snapshot's content
-
-    // Create the content to show
-    const content = new vscode.MarkdownString();
-    relevantTestSnap.forEach(snp => {
-      if (mockExplorer === undefined) {
-        return;  // Can't verify mocks actually exist
-      }
-      const mockLocation = mockExplorer.getPathFromHash(snp.hash);
-      if (mockLocation === undefined) {
-        return; // Filter mocks that aren't found in the explorer
-      }
-      const cmdUri = vscode.Uri.parse(`command:unmock.editMock?${
-        encodeURIComponent(JSON.stringify({currentPath: mockLocation}))
-      }`);
-      const md = `[\`${snp.hash}\`](${cmdUri}): _${snp.method.toUpperCase()}_ ` +
-                 `[${(snp.host + snp.path).substr(0, 24) + "..."}]()  \n`;
-      content.appendMarkdown(md);
+    return buildLinkTestsMocksHover({
+      relevantSnap: snap,
+      testIndex: matchingTestIndex,
+      range: relevantRanges[matchingTestIndex],
     });
-    content.isTrusted = true;
-    return new vscode.Hover(content);
   }
 }
 
 // Defined as functions (and not arrow notation) so these are available anywhere in the document
-
-const SNAPSHOT_SUFFIX = ".snap";
-// These must be found in a snapshot to be considered valid
-const SNAPSHOT_KEYWORDS = ["\"hash\":", "\"host\":", "\"method\":", "\"path\":"];
+function buildLinkTestsMocksHover(...args: IBuildLinkTestMockHover[]) {
+  if (args.length === 0) {
+    return;
+  }
+  const { relevantSnap, testIndex, range } = args[0];
+  const relevantTestSnap = relevantSnap.loadTest({ idx: testIndex });
+  // Create the content to show
+  const content = new vscode.MarkdownString();
+  relevantTestSnap.forEach(snp => {
+    if (mockExplorer === undefined) {
+      return; // Can't verify mocks actually exist
+    }
+    const mockLocation = mockExplorer.getPathFromHash(snp.hash);
+    if (mockLocation === undefined) {
+      return; // Filter mocks that aren't found in the explorer
+    }
+    const cmdUri = vscode.Uri.parse(
+      `command:unmock.editMock?${encodeURIComponent(JSON.stringify({ currentPath: mockLocation }))}`
+    );
+    const md =
+      `[\`${snp.hash}\`](${cmdUri}): _${snp.method.toUpperCase()}_ ` +
+      `[${(snp.host + snp.path).substr(0, 24) + "..."}]()  \n`;
+    content.appendMarkdown(md);
+  });
+  content.isTrusted = true;
+  return new vscode.Hover(content, range);
+}
 
 function countBracesInLine(line: string) {
-  return countInString(line, '{') - countInString(line, '}');
+  return countInString(line, "{") - countInString(line, "}");
 }
 
 function findBalancedCurlyBraces(location: vscode.Range, srcText: string[]): vscode.Range {
@@ -149,10 +199,13 @@ function findBalancedCurlyBraces(location: vscode.Range, srcText: string[]): vsc
   let lineSpan = 0; // How many lines we traversed already
   let bracesCount = -1; // How many braces we encountered, -1 signifies start of loop
 
-  for (const line of lines) { // Iterate over lines, searching for braces
+  for (const line of lines) {
+    // Iterate over lines, searching for braces
     const lineWithoutStrings = removeStringsFromSourceText(line); // Ignore braces found in strings...
-    if (bracesCount === -1) {  // Look for matching first brace first!
-      if (lineWithoutStrings.indexOf('{') !== -1) {  // The line indeed has '{' - otherwise we keep searching for it...
+    if (bracesCount === -1) {
+      // Look for matching first brace first!
+      if (lineWithoutStrings.indexOf("{") !== -1) {
+        // The line indeed has '{' - otherwise we keep searching for it...
         bracesCount = countBracesInLine(lineWithoutStrings);
       }
     } else {
@@ -182,18 +235,23 @@ function findBalancedCurlyBraces(location: vscode.Range, srcText: string[]): vsc
   const endLine = lines[lineSpan];
   let endLocation = 0;
   while (bracesToIgnore > -1) {
-    endLocation = endLine.indexOf('}', endLocation);
+    endLocation = endLine.indexOf("}", endLocation);
     bracesToIgnore -= 1;
   }
 
   // endLocation + 1 to include '}'
-  return new vscode.Range(location.start.line, location.start.character,
-                          location.start.line + lineSpan, endLocation + 1);
+  return new vscode.Range(
+    location.start.line,
+    location.start.character,
+    location.start.line + lineSpan,
+    endLocation + 1
+  );
 }
 
 function findRangesFromTestNames(rawContent: string, uniqueTests: string[]) {
   const splitContent = rawContent.split("\n");
   const testNameLocations = getRangeFromTextAndMatch(rawContent, uniqueTests, false);
   // returned range spans from beginning of the given line to the location of matching '}'...
+  // TODO - should this be extend to capture e.g. the `test`/`it`/`describe` portion?
   return testNameLocations.map(location => findBalancedCurlyBraces(location, splitContent));
 }

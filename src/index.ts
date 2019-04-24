970d0ef5c0326cee7b0e1b488b9424ffc8e6c92a
Idan Tene <12184618+idantene@users.noreply.github.com>
2019-04-24 17:44:30 +0300

    Codelens assoc (#16)
    * Adds tslint and prettifier
    
    * Show hover when clicking on codelens

--- src/index.ts
+++ src/index.ts
@@ -3,34 +3,23 @@ import * as fs from "fs";
 import * as path from "path";
 import * as ini from "ini";
 import * as os from "os";
-import { IInsertUnmockAction, IMockLocation } from "./interfaces";
+import { debounce } from "debounce";
+import { IMockLocation } from "./interfaces";
 import { MockExplorer } from "./explorer";
-import { TypescriptInsertUnmockCodeLens,
-         TypeScriptInsertUnmockAction,
-         InsertUnmockHoverProvider } from "./providers/insert-unmock";
-import { LinkMockHoverProvider } from "./providers/link-tests-mocks";
-import { getImportStatement, getTestCalls, getConfig, AllJSFileFilters } from "./utils";
-
-const insertUnmockToTest = (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: IInsertUnmockAction[]) => {
-  // Adds unmock to a test file
-  if (args.length === 0) { // No args given
-    return;
-  }
-  const argsObj = args[0];
-  // Adds the import statement in the specified `unmockImportLocation` under `args`
-  textEditor.insertSnippet(new vscode.SnippetString(`${getImportStatement(argsObj.lang)}\n`),
-                           argsObj.unmockImportLocation);
-  const lastImportLocation = argsObj.lastImportLocation;
-  // Add the beforeEach and afterEach calls after the last import
-  // +1 to add after the last import statement, +1 to account for the addition of the unmock import
-  const afterLastImport = new vscode.Range(lastImportLocation.line + 2, 0, lastImportLocation.line + 2, 0);
-  textEditor.insertSnippet(new vscode.SnippetString(`\n${getTestCalls(argsObj.lang)}\n`), afterLastImport);
-};
+import {
+  TypescriptInsertUnmockCodeLens,
+  TypeScriptInsertUnmockAction,
+  InsertUnmockHoverProvider,
+  insertUnmockToTest,
+} from "./providers/insert-unmock";
+import { LinkMockHoverProvider, LinkMockCodeLens, SnapCollection } from "./providers/link-tests-mocks";
+import { getConfig, AllJSFileFilters } from "./utils";
 
 const updateRefreshToken = (config: vscode.WorkspaceConfiguration) => {
   let refreshToken: string | undefined;
   const homeDirs = [];
-  if (vscode.workspace.rootPath !== undefined) { // First preference to workspace home directory
+  if (vscode.workspace.rootPath !== undefined) {
+    // First preference to workspace home directory
     homeDirs.push(vscode.workspace.rootPath);
   }
   homeDirs.push(os.homedir()); // Second preference to user home directory
@@ -40,7 +29,7 @@ const updateRefreshToken = (config: vscode.WorkspaceConfiguration) => {
     if (!foundCredentialsFile) {
       const credentialFile = path.join(homedir, unmockPath, "credentials");
       if (fs.existsSync(credentialFile)) {
-        const iniContents = ini.parse(fs.readFileSync(credentialFile, 'utf-8'));
+        const iniContents = ini.parse(fs.readFileSync(credentialFile, "utf-8"));
         refreshToken = iniContents.unmock.token;
         foundCredentialsFile = true;
       }
@@ -52,43 +41,109 @@ const updateRefreshToken = (config: vscode.WorkspaceConfiguration) => {
 
 export let mockExplorer: undefined | MockExplorer;
 
+const disposables: vscode.Disposable[] = [];
+
 export function activate(context: vscode.ExtensionContext) {
   const config = getConfig();
 
-  vscode.commands.registerTextEditorCommand("unmock.insertUnmockToTest", insertUnmockToTest);
+  /* TODO
+   * Saved in code for future reference (while still in `dev` - please delete me before merging to `master`!)
+   * Example of how to add (with CSS) comments in-line with the code.
+   * Supports markdown as needed; this could be a good replacement for codelens and less intrusive; doesn't affect vertical space.
+   * To use without slowing down the IDE, should use debounce.
+   *
+   * const t = vscode.window.createTextEditorDecorationType({
+   *   after: {
+   *     margin: "0 0 0 3em",
+   *     textDecoration: "none",
+   *   },
+   *   rangeBehavior: vscode.DecorationRangeBehavior.ClosedOpen,
+   * });
+   * vscode.window.onDidChangeTextEditorSelection(
+   *   debounce((e: vscode.TextEditorSelectionChangeEvent) => {
+   *     const editor = e.textEditor;
+   *     const endPosition = e.selections[0].active.with(undefined, Number.MAX_SAFE_INTEGER);
+   *     editor.setDecorations(t, [
+   *       {
+   *         hoverMessage: new vscode.MarkdownString("## What is this").appendMarkdown("\nthat this is"),
+   *         range: new vscode.Range(endPosition, endPosition),
+   *         renderOptions: {
+   *           after: {
+   *             contentText: "spameggs",
+   *           },
+   *         },
+   *       },
+   *     ]);
+   *   }, 250)
+   * );
+   */
 
-  if (config.codeLens === true) {  // Add the CodeLens suggestions (if enabled)
-    vscode.languages.registerCodeLensProvider(AllJSFileFilters, new TypescriptInsertUnmockCodeLens());
+  // This is needed for the Uri used in markdowns
+  disposables.push(vscode.commands.registerTextEditorCommand("unmock.insertUnmockToTest", insertUnmockToTest));
+
+  disposables.push(
+    vscode.commands.registerTextEditorCommand(
+      "unmock.showMockLinkHover",
+      (editor: vscode.TextEditor, edit: vscode.TextEditorEdit, ...args: any[]) => {
+        const { range } = args[0];
+        // Somewhat hackish. To show the markdown-supporting Hover, the cursor (not mouse) needs to be inside the relevant range.
+        // To not-interrupt user action, effectively move the cursor to relevant position, call the showHover, and reset cursor selection.
+        const curSelection = editor.selection;
+        editor.selection = new vscode.Selection(range.start, range.start);
+        vscode.commands.executeCommand("editor.action.showHover").then(() => (editor.selection = curSelection));
+      }
+    )
+  );
+
+  if (config.codeLens === true) {
+    // Add the CodeLens suggestions (if enabled)
+    disposables.push(vscode.languages.registerCodeLensProvider(AllJSFileFilters, new TypescriptInsertUnmockCodeLens()));
   }
-  if (config.lightbulb === true) { // Add the lightbulb suggestions (if enabled)
-    vscode.languages.registerCodeActionsProvider(AllJSFileFilters, new TypeScriptInsertUnmockAction());
+  if (config.lightbulb === true) {
+    // Add the lightbulb suggestions (if enabled)
+    disposables.push(
+      vscode.languages.registerCodeActionsProvider(AllJSFileFilters, new TypeScriptInsertUnmockAction())
+    );
   }
   // Register hover providers
-  vscode.languages.registerHoverProvider(AllJSFileFilters, new InsertUnmockHoverProvider());
-  vscode.languages.registerHoverProvider(AllJSFileFilters, new LinkMockHoverProvider());
+  disposables.push(vscode.languages.registerHoverProvider(AllJSFileFilters, new InsertUnmockHoverProvider()));
+
+  // For linking mocks and tests, we need a collection of snap files
+  const snapCollection = new SnapCollection(); // todo - cleanup?
+  disposables.push(vscode.languages.registerHoverProvider(AllJSFileFilters, new LinkMockHoverProvider(snapCollection)));
+  disposables.push(vscode.languages.registerCodeLensProvider(AllJSFileFilters, new LinkMockCodeLens(snapCollection)));
 
   // Add the extension button
-  mockExplorer = new MockExplorer();
-  vscode.window.registerTreeDataProvider("unmock.mocksExplorer", mockExplorer);
+  mockExplorer = new MockExplorer(); // todo - cleanup?
+  disposables.push(vscode.window.registerTreeDataProvider("unmock.mocksExplorer", mockExplorer));
 
   // Add individual commands
-	vscode.commands.registerCommand("unmock.editMock", (element: IMockLocation) => {  // Open mock from MockTreeItem
-		vscode.commands.executeCommand("vscode.open", vscode.Uri.file(element.currentPath));
-  });
+  disposables.push(
+    vscode.commands.registerCommand("unmock.editMock", (element: IMockLocation) => {
+      // Open mock from MockTreeItem
+      vscode.commands.executeCommand("vscode.open", vscode.Uri.file(element.currentPath));
+    })
+  );
 
-	// Create the statusbar section
-	const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-	vscode.commands.registerCommand("unmock.statusbar.hide", () => statusBar.hide());
-	vscode.commands.registerCommand("unmock.statusbar.text", (text: string) => {
-		statusBar.text = `Unmock: ${text}`;
-		statusBar.show();
-	});
-	statusBar.command = "unmock.statusbar.hide";
+  // Create the statusbar section
+  const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+  disposables.push(statusBar);
+  disposables.push(vscode.commands.registerCommand("unmock.statusbar.hide", () => statusBar.hide()));
+  disposables.push(
+    vscode.commands.registerCommand("unmock.statusbar.text", (text: string) => {
+      statusBar.text = `Unmock: ${text}`;
+      statusBar.show();
+    })
+  );
+  statusBar.command = "unmock.statusbar.hide";
 
-  if (config.refreshToken === null) { // Not set in configuration, look for it in other places
+  if (config.refreshToken === null) {
+    // Not set in configuration, look for it in other places
     updateRefreshToken(config);
-	}
+  }
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+// this method is called when the extension is deactivated
+export function deactivate() {
+  disposables.forEach(disp => disp.dispose());
+}
